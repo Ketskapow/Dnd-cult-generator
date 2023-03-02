c6a90103083f92b5a222bdac3756fdd8bf1e2267
Ketskapow <danielscheeper@gmail.com>
2023-03-02 18:53:40 +0100

    Finished person generator with proper races and genders, added some races that were still missing, improved readability.

--- persongenerator.py
+++ persongenerator.py
@@ -1,9 +1,29 @@
 import random
 
 # Define race distribution
-races = ['Human', 'Elf', 'Dwarf', 'Halfling', 'Half-Elf', 'Half-Orc', 'Gnome', 'Dragonborn', 'Tiefling', 'Minotaur', 'Triton', 'Aasimar', 'Goliath', 'Kenku', 'Yuan-ti Pureblood', 'Tabaxi', 'Air Genasi', 'Water Genasi', 'Fire Genasi']
-race_distribution = [0.30, 0.15, 0.10, 0.10, 0.10, 0.05, 0.05, 0.05, 0.03, 0.02, 0.02, 0.02, 0.02, 0.02, 0.01, 0.01, 0.05, 0.02, 0.02]\
+race_dictionary = {
+    'Human': 0.25,
+    'Elf': 0.15,
+    'Dwarf': 0.10,
+    'Halfling': 0.10,
+    'Half-Elf': 0.10,
+    'Half-Orc': 0.05,
+    'Gnome': 0.05,
+    'Dragonborn': 0.05,
+    'Tiefling': 0.03,
+    'Minotaur': 0.03,
+    'Triton': 0.02,
+    'Aasimar': 0.02,
+    'Goliath': 0.02,
+    'Kenku': 0.02,
+    'Yuan-ti Pureblood': 0.01,
+    'Tabaxi': 0.01,
+    'Genasi': 0.02
+}
 
+# create a list of races with probabilities
+races = list(race_dictionary.keys())
+race_distribution = list(race_dictionary.values())
 
 # Define age range for each race
 age_range = {
@@ -28,6 +48,8 @@ age_range = {
 
 gender_options = ['Male', 'Female', 'Nonbinary']
 
+gendered_races = ['Human', 'Elf', 'Dwarf', 'Halfling', 'Half-Elf', 'Half-Orc', 'Gnome', 'Dragonborn', 'Tiefling', 'Minotaur', 'Triton', 'Aasimar', 'Goliath']
+
 # Generate 10 persons
 persons = []
 for i in range(70):
@@ -40,13 +62,32 @@ for i in range(70):
     #Generate gender based on options
     gender = random.choices(gender_options)[0]
     
-    if race in ['Human', 'Elf', 'Dwarf', 'Halfling', 'Dragonborn', 'Tiefling', 'Half-Orc', 'Aasimar', 'Gnome', 'Goliath'] and gender in ['Male', 'Female']:
+    #If gender is male or female, and race is male or female, pull name from corresponding text file.
+    if race in gendered_races and gender in ['Male', 'Female']:
         filename = f"data/{race}_{gender}.txt"
         with open(filename) as f:
             names_list = f.readlines()
         name = random.choice(names_list).strip()
-    else:
-        name = f"{race} {gender}"
+    #If gender is nonbinary, but race is gendered, pull name form either text file    
+    elif race in gendered_races and gender == 'Nonbinary':
+        filename = f"data/{race}_{random.choices(['Male','Female'])[0]}.txt"
+        with open(filename) as f:
+            names_list = f.readlines()
+        name = random.choice(names_list).strip()
+    #If race is ungendered, pull name from race file    
+    elif race in ['Tabaxi', 'Yuan-ti Pureblood', 'Kenku']:
+        filename = f"data/{race}.txt" 
+        with open(filename) as f:
+            names_list = f.readlines()
+        name = random.choice(names_list).strip()
+    #If race is genasi, first generate element, then take name from corresponding file, then update race.
+    elif race is 'Genasi':
+        element = random.choice(['Air', 'Water', 'Fire'])
+        filename = f"data/{race}_{element}.txt" 
+        with open(filename) as f:
+            names_list = f.readlines()
+        name = random.choice(names_list).strip()
+        race = element + " " + race  
 
     # Add person to list
     person = {'name': name, 'age': age, 'race': race, 'gender': gender}
